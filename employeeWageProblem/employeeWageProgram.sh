53786f75a0a0b1b12457f4c54d4e133a7434fe9f
sumit <st.sumit01@gmail.com>
2020-02-13 17:46:35 +0530

    [sumit]Refactor . added case - 4 in case statement for monthly wage

--- employeeWageProblem/employeeWageProgram.sh
+++ employeeWageProblem/employeeWageProgram.sh
@@ -28,7 +28,10 @@ function CaseStatement()
 			 echo "part time wage : $parttimewage"
 			 ;;
 		4)
-
+			CalculateWagePerMonth
+			monthlywage=$?
+			echo "monthly wage : $monthlywage"
+			;;
 	 	0)
 			exit 0 
 			;;

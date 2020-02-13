3d1e8c37b9560ca2dfd973ab0debf87f594c7fec
sumit <st.sumit01@gmail.com>
2020-02-13 10:43:29 +0530

    [sumit]Refactor . removed the one duplicate function

--- employeeWageProblem/employeeWageProgram.sh
+++ employeeWageProblem/employeeWageProgram.sh
@@ -1,28 +1,9 @@
 
-
-
-
 #!/bin/bash
 set -x
 # employee wage computation program implementation
 echo " welcome to the employee wage computation program "
-
- function AttendanceCheck()
-  {
-          local present=$(( $RANDOM % 20 ))
-          if [ "$present" != "0" ]
-          then
-                  echo "employee is present"
-          else
-                  echo "employee is absent"
-          fi
-          return $present
-  }
-
-
-
-
-
+ 
 function AttendanceCheck()
 {
 	local present=$(( $RANDOM % 20 ))

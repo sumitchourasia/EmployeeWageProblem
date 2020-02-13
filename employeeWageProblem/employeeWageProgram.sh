f80c19d33f9de65873c9ba3faa5341ce64567abf
sumit <st.sumit01@gmail.com>
2020-02-13 13:01:23 +0530

    [sumit]Add . adding branch usecase-3 with function PartTimeWage

--- employeeWageProblem/employeeWageProgram.sh
+++ employeeWageProblem/employeeWageProgram.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-set -x
-# employee wage computation program implementation
-echo " welcome to the employee wage computation program "
+function PartTimeWage()
+{
+        part_time_hour=8
+        wage_per_hour=20
+        part_time_wage=`expr $part_time_hour \* $wage_per_hour`
+        return $part_time_wage
+}
 

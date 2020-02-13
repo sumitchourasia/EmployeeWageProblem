04fc2a1a9f55fe5cf3cd098ebfa908705017291a
sumit <st.sumit01@gmail.com>
2020-02-13 12:55:05 +0530

    [sumit]Refactor . restoring to the last commit

--- employeeWageProblem/employeeWageProgram.sh
+++ employeeWageProblem/employeeWageProgram.sh
@@ -15,3 +15,12 @@ function AttendanceCheck()
 	return $present
 }
 
+function CalculateDailyEmployeeWage()
+{
+        wageperhour=20
+        hoursperday=8
+        dailywage=`expr $wageperhour \* $hoursperday`
+        return $dailywage
+}
+
+

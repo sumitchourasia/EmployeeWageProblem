82a17dc03cc0a8f58b3ca1d66e5eec1e7afc7dbc
sumit <st.sumit01@gmail.com>
2020-02-14 12:49:46 +0530

    [sumit]Add . adding new branch useCase-8 with function DailyWageAndTotalWage

--- employeeWageProblem/employeeWageProgram.sh
+++ employeeWageProblem/employeeWageProgram.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-set -x
-# employee wage computation program implementation
-echo " welcome to the employee wage computation program "
+
+function DailyWageAndTotalWage()
+{
+	CalculateDailyEmployeeWage
+	dailywage=$?
+	CalculateWagePerMonth
+	echo "Daily Wage : $dailywage " >> ./employeeWageProblem/DailyWageAndTotalWage.txt
+	echo "Total Wage : $monthlywage " >> ./employeeWageProblem/DailyWageAndTotalWage.txt
+}
 
